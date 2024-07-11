823d22466676dc9fc60663bbb411b299d92d826f
Sofia Victoria <sofiavictoria91@gmail.com>
2024-07-11 19:58:01 -0400

    carrusel, secciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,20 @@
 $(document).ready(
     function () {
 
-    $('#tours').tooltip();
+        $('#tours').tooltip();
 
-});
+
+        $("a").on('click', function (event) {
+            if (this.hash !== "") {
+                event.preventDefault();
+                var hash = this.hash;
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800, function () {
+
+                    window.location.hash = hash;
+                });
+            }
+        });
+
+    });
