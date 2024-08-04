741e3343cbb1f6715e94cf8b82ba2603beae896d
Sofia Victoria <sofiavictoria91@gmail.com>
2024-08-03 20:07:27 -0400

    Cambios a blog original, inclusions de sass, bem, instalación de boostrap y elementos js

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
