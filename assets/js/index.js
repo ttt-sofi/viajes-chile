20ebb8df938b6e1437ba302f133ee90d18db89a4
Sofia Victoria <sofiavictoria91@gmail.com>
2024-08-05 21:07:53 -0400

    alerta mensaje enviado

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,21 @@ import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+document.addEventListener('DOMContentLoaded', function () {
+    const submitBtn = document.getElementById('submitBtn');
+    const messageBox = document.getElementById('messageBox');
+    
+    submitBtn.addEventListener('click', function (event) {
+        event.preventDefault();  // Prevenir el envío del formulario para mostrar el mensaje
+        
+        // Crear el mensaje
+        const message = document.createElement('div');
+        message.className = 'alert alert-success';
+        message.textContent = 'Mensaje enviado';
+
+        // Insertar el mensaje en el messageBox
+        messageBox.innerHTML = ''; // Limpiar cualquier mensaje previo
+        messageBox.appendChild(message);
+    });
+});
